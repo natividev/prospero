02a3528c831b53617c97f128b16bbcbf3e6a2125
natividev <josenatividadcv@gmail.com>
2024-01-23 10:12:09 -0600

    Add AuthModule and AuthController

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const APP_ROUTE_PREFIX = 'api';
+  app
+    .enableVersioning({
+      defaultVersion: '1',
+      type: VersioningType.URI,
+    })
+    .setGlobalPrefix(APP_ROUTE_PREFIX);
+
   await app.listen(3000);
 }
 bootstrap();
